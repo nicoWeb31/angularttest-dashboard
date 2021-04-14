52b79c0d12f09ced3ac8df8f1ffc1b6d9b7eb5fa
NicoRiot <nriot@umoove-inc.com>
2021-04-14 21:39:51 +0200

    register login page

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  mail: string = '';
+  password: string = '';
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    
   }
 
 }
